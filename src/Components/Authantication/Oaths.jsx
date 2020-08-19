84bcab9a43111b39d4bf9a93787078b621357332
root <p2012agarwal@gmail.com>
2020-08-19 21:17:52 +0530

    added client id

--- src/Components/Authantication/Oaths.jsx
+++ src/Components/Authantication/Oaths.jsx
@@ -3,13 +3,13 @@ import GoogleLogin from 'react-google-login';
 
 const clientid = {
                     production: "",
-                    local: ""
+                    local: "728214676777-r5iucch4vtbpqgjtivo54td6acfk6jam.apps.googleusercontent.com"
                 }
 
 const googlebtn = (responseGoogle,btntext) =>{
     return(
         <GoogleLogin
-                clientId={clientid.heroku}
+                clientId={clientid.local}
                 render={renderProps => (
                     <button onClick={renderProps.onClick}><i className="fa fa-google"> <span className='authname'>{btntext}</span></i></button>
                 )}
