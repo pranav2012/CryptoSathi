49dc3fee9effe448637bbd66cc0706bbbe0489b2
root <p2012agarwal@gmail.com>
2020-08-28 00:47:37 +0530

    mail feature

--- CryptoSathi_backend/authantication/google.js
+++ CryptoSathi_backend/authantication/google.js
@@ -1,38 +1,63 @@
-const googleregister = (req,res,db,bcrypt) =>{
-    const {name,email,username} = req.body;
+const nodemailer = require('nodemailer');
+
+
+let mailTransporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'p2012agarwal@gmail.com',
+        pass: '2001pranav'
+    }
+});
+
+const googleregister = (req, res, db, bcrypt) => {
+    const { name, email, username } = req.body;
     const hash = bcrypt.hashSync(email);
     db('users')
-    .returning('*')
-    .insert({
-        name: name,
-        email: email,
-        joined: new Date(),
-        username: username,
-        hash: hash
-    }).then(result => {
-        db.select('id', 'email', 'name').from('users').where('email', '=', email)
-        .then(user=>{
+        .returning('*')
+        .insert({
+            name: name,
+            email: email,
+            joined: new Date(),
+            username: username,
+            hash: hash
+        }).then(result => {
+            db.select('id', 'email', 'name').from('users').where('email', '=', email)
+                .then(user => {
+                    res.json({
+                        status: 'sucess',
+                        username: user[0].name,
+                        id: user[0].id,
+                    });
+                    let mailDetails = {
+                        from: 'pranav@CryptoSathi.com',
+                        to: user[0].email,
+                        subject: 'Welcome, to CryptoSathi',
+                        text: `Hey ${user[0].name}, Thanks for registering!`
+                    };
+
+                    mailTransporter.sendMail(mailDetails, function (err, data) {
+                        if (err) {
+                            console.log('Error Occurs');
+                        } else {
+                            console.log('Email sent successfully');
+                        }
+                    });
+                }).catch(err => res.status(400).json('no such user'));
+        })
+        .catch(err => res.status(400).json('error'));
+}
+
+const googleauthchecker = (req, res, db) => {
+    const { email } = req.body;
+    db.select('id', 'email', 'name').from('users').where('email', '=', email)
+        .then(user => {
             res.json({
                 status: 'sucess',
                 username: user[0].name,
                 id: user[0].id,
             });
-        }).catch(err=> res.status(400).json('no such user'));
-    })
-    .catch(err => res.status(400).json('error'));
-}
-
-const googleauthchecker = (req,res,db) => {
-    const {email} = req.body;
-    db.select('id', 'email', 'name').from('users').where('email', '=', email)
-    .then(user=>{
-        res.json({
-            status: 'sucess',
-            username: user[0].name,
-            id: user[0].id,
-        });
-    })
-   .catch(err => res.status(400).json('error'));
+        })
+        .catch(err => res.status(400).json('error'));
 }
 
 module.exports = {
